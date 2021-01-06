db81363d3ed318164c2231247ad3fe131d824add
Unknown <un12known34_acc@outlook.com>
2021-01-06 17:18:49 +1000

    Lets hope this works

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    private bool isFired = false;
+
+    void OnTriggerStay()
+    {
+        AttachArrow();
+    }
+    
+    void OnTriggerEnter()
+    {
+        //UnityEngine.Debug.Log("Entered Trigger Zone");
+        AttachArrow();
+    }
+
+    void Update()
+    {
+        if (isFired)
+        {
+            transform.LookAt(transform.position + transform.GetComponent<Rigidbody>().velocity);
+        }
+    }
+
+    public void Fired()
+    {
+        isFired = true;
+    }
+
+    private void AttachArrow()
+    {
+        var device = SteamVR_Controller.Input((int)ArrowManager.Instance.trackedObj.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            ArrowManager.Instance.AttachBowToArrow();
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class ArrowManager : MonoBehaviour {
+
+    public static ArrowManager Instance;
+    
+    public SteamVR_TrackedObject trackedObj;
+    
+    private GameObject currentArrow;
+
+    public GameObject stringAttachPoint;
+    public GameObject arrowStartPoint;
+    public GameObject stringStartPoint;
+
+    public GameObject arrowPrefab;
+
+    private bool isAttached = false;
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if(Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AttachArrow();
+        PullString();
+    }
+
+    private void PullString()
+    {
+        if (isAttached)
+        {
+            float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+            stringAttachPoint.transform.localPosition = stringStartPoint.transform.localPosition -  new Vector3(0.0f, 5 *  dist, 0.0f);
+
+            var device = SteamVR_Controller.Input((int)trackedObj.index);
+            if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                //ArrowManager.Instance.AttachBowToArrow();
+                Fire();
+            } 
+        }
+    }
+
+    private void Fire()
+    {
+        float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+
+        currentArrow.transform.parent = null;
+        currentArrow.GetComponent<Arrow>().Fired();
+
+        Rigidbody r = currentArrow.GetComponent<Rigidbody>();
+        r.velocity = currentArrow.transform.forward * 25.0f * dist;
+        r.useGravity = true;
+
+        stringAttachPoint.transform.position = stringStartPoint.transform.position;
+
+        currentArrow = null;
+        isAttached = false;
+
+        stringStartPoint.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    private void AttachArrow()
+    {
+        if (currentArrow == null)
+        {
+            currentArrow = Instantiate(arrowPrefab);
+            currentArrow.transform.parent = trackedObj.transform;
+            currentArrow.transform.localPosition = new Vector3(0.0f, 0.0f, 0.342f);
+            currentArrow.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    public void AttachBowToArrow()
+    {
+        currentArrow.transform.parent = stringAttachPoint.transform;
+        currentArrow.transform.localPosition = arrowStartPoint.transform.localPosition;
+        currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
+
+        isAttached = true;
+    }
+}
